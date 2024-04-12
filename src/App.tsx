6d22c9e974dee829a39fdd2af8b3fe7ed0d904ce
noodleybroodley <educatedgraph@gmail.com>
2024-04-12 13:23:59 -0400

    added babel to dev dependencies to remove a console warning

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 
-export function App() {
+function App() {
   return (
     <div className="App">
       <header className="App-header">
