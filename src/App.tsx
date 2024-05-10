c15eff1c57bad6f907c3691152772cc5deb8ecb7
noodleybroodley <educatedgraph@gmail.com>
2024-05-10 18:16:20 -0400

    added choose file button functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,36 @@
 import CameraAltIcon from '@mui/icons-material/CameraAlt';
 import './App.css'
+import { useState } from 'react';
 
 export default function App() {
+  const [file, setFile] = useState<File>();
+  //define settings for file picker
+  const pickerOpts = {
+    types: [
+      {
+        description: "Images",
+        accept: {
+          "image/*": [".png", ".jpeg", ".jpg"],
+        },
+      },
+    ],
+    excludeAcceptAllOption: true,
+    multiple: false,
+  };
+  
+  /** Opens the system file picker and saves it to the "file" variable.
+   * @param none
+   * @returns none
+  */
+  async function getTheFile() {
+    // Open file picker and destructure so to grab the first element of the returned list
+    //@ts-ignore
+    const [fileHandle] = await window.showOpenFilePicker(pickerOpts);
+  
+    // get file contents
+    const fileData = await fileHandle.getFile();
+    setFile(fileData);
+  }
 
   return (
     <div className="Homepage">
@@ -10,7 +39,7 @@ export default function App() {
         <CameraAltIcon data-testid="logo" fontSize='inherit'/>
       </div>
       <div className="Subtitle">The Simple Image Converter</div>
-      <button className="ChooseFileButton">Choose File...</button>
+      <button className="ChooseFileButton" onClick={getTheFile}>Choose File...</button>
     </div>
   )
 }
